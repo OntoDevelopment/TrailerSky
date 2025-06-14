c04bcb268ba9e46d0bbfccf1ccb31383036f748d
Brian Wendt <brian.m.wendt@gmail.com>
2025-06-13 21:53:05 -0700

    added import_video action

--- app/Orchid/Screens/Videos/VideoListScreen.php
+++ app/Orchid/Screens/Videos/VideoListScreen.php
@@ -4,6 +4,7 @@ namespace App\Orchid\Screens\Videos;
 
 use App\Models\Video;
 use App\Orchid\Screens\AppScreen;
+use Illuminate\Support\Facades\Request;
 
 class VideoListScreen extends AppScreen
 {
@@ -32,5 +33,21 @@ class VideoListScreen extends AppScreen
         ];
 
         return $layout;
+    }
+
+    public function import_video(Request $request)
+    {
+        $youtube = $request->input('youtube_url');
+        $url_parts = parse_url($youtube);
+        parse_str($url_parts['query'], $query_params);
+        if(!$query_params['v']){
+            return redirect()->back()->with('error', 'Invalid YouTube URL');
+        }
+        $action = new \App\Actions\YouTube\ImportVideo;
+        $action->run(['id' => $query_params['v']]);
+        if($action->hasError()){
+            return redirect()->back()->with('error', 'Error importing video');
+        }
+        return redirect()->back()->with('success', 'Video imported successfully');
     }
 }
