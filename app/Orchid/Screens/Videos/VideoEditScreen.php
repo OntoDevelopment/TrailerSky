62e4c3098ab4285a2d43be06848572a56f9bc607
Brian Wendt <brian.m.wendt@gmail.com>
2025-10-09 17:18:29 -0700

    removed unnecessary refresh

--- app/Orchid/Screens/Videos/VideoEditScreen.php
+++ app/Orchid/Screens/Videos/VideoEditScreen.php
@@ -67,7 +67,6 @@ class VideoEditScreen extends Screen
         $video->type = $request->input('video.type');
         $video->save();
         Toast::info('Video was saved');
-        return redirect()->refresh();
     }
 
     public function setTMDB(Video $video, Request $request)
