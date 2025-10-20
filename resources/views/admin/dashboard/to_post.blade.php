a8494171e84e79402b03a42bfc00115a6a30e78f
Brian Wendt <brian.m.wendt@gmail.com>
2025-10-20 09:56:25 -0700

    added display of language when not English

--- resources/views/admin/dashboard/to_post.blade.php
+++ resources/views/admin/dashboard/to_post.blade.php
@@ -16,6 +16,9 @@
                 </td>
                 <td>
                     <a href="{{ $Video->media->tmdb_url }}" target="_blank">TMDB</a> | Popularity: {{ $Video->media->tmdb_popularity }}
+                    @if($Video->media->original_language != 'en')
+                        <br />Language: {{ iso_639_1($Video->media->original_language) ?? 'unknown' }}
+                    @endif
                 </td>
                 <td>
                     <div class="btn-group">
